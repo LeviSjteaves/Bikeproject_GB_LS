a3d08575b4ce8d40f91da736d5712d9c6080b1a2
Gaizkab <gaizkabarrasa11@gmail.com>
2023-02-23 09:43:13 +0100

    23/02-ReferenceTest_v1

---
+++ referenceTest.m
@@ -0,0 +1,30 @@
+function [initialization_new] = referenceTest(traj,Th,Ts,initialization)
+    % Check if the trajectory is valid
+    traj_new = traj;
+
+    % Minimun number of reference points
+    if N<(Th/Ts)
+        disp('Not enough reference points');
+
+    end
+    
+    % Initialization errors
+    if traj(1,1) ~= initialization(1,1) || traj(1,2) ~= initialization(1,2) || traj(1,3) ~= initialization(1,3)
+        disp('initialization is not correct');
+        initialization_new(1,1) = traj(1,1);
+        initialization_new(1,2) = traj(1,2);
+        initialization_new(1,3) = traj(1,3);
+    end
+        
+        
+        
+
+
+
+
+
+
+
+
+
+end
