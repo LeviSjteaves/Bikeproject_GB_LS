794add2ffcf8d4e920d2ffecb14dc3a418d2da01
LeviSjteaves <l.a.stevens@student.tue.nl>
2023-02-23 09:48:02 +0100

    23/2 Merged

--- referenceTest.m
+++ referenceTest.m
@@ -16,15 +16,13 @@ function [initialization_new] = referenceTest(traj,Th,Ts,initialization)
         initialization_new(1,3) = traj(1,3);
     end
         
+    % Density of datapoints
+
+Min_density_distance = 1;
+
+traj(n,1)  
+if dist > Min_density_distance
+    
+end    
         
-        
-
-
-
-
-
-
-
-
-
 end
