cf1adbb83a04f963cae0c50d3aa1f14e43e34159
Gaizkab <gaizkabarrasa11@gmail.com>
2023-03-03 09:32:37 +0100

    3/03- Test_ss file added
    Added the file where we test the A matrix and the Ax+Bu state space model

---
+++ Test_ss.m
@@ -0,0 +1,72 @@
+clear all;
+clc;
+
+% Params
+Ts = 0.01;
+v = 3; 
+h = 0.534;
+b = 1.15;
+a = 0.6687;
+lambda = deg2rad(66);
+c = 0.06;
+m = 31.3;
+g = 9.81;
+
+lr = b; % distance from rear wheel center to center of mass
+lf = b-a; % distance from front wheen center to center of mass
+
+% States in global frame
+States = [100 10 0.1 0.1 0.1 0.1 3]'; 
+
+% States in local frame
+States_l = zeros(7,1);
+
+States_l(1) = States(1) * cos(States(3)) + States(2) * sin(States(3));
+States_l(2) = -States(1) * sin(States(3)) + States(2) * cos(States(3));
+
+States_l(3) = 0;        % direction of bike is aligned with X local frame
+States_l(4) = States(4);
+States_l(5) = States(5);
+States_l(6) = States(6);
+States_l(7) = States(7);
+
+% Input value (= 0  if A wants to be tested, != if Ax+Bu wants to be
+% tested)
+dot_delta_e = 3;
+
+%% Matlab script to obtain Kalman gain
+
+% A matrix (linear bicycle model with constant velocity)
+A = [0 0 0 0 0 0 1;
+     0 0 v 0 0 v*(lr/(lf+lr)) 0;
+     0 0 0 0 0 (v/(lr+lf)) 0;
+     0 0 0 0 1 0 0;
+     0 0 0 (g/h) ((v^2*h-lr*g*c)/(h*(lr+lf))) 0 0;
+     0 0 0 0 0 0 0;
+     0 0 0 0 0 0 0];
+
+% B matrix (linear bicycle model with constant velocity)
+B = [0 0 0 0 ((lr*v)/(h^2*(lr+lf))) 1 0]';
+
+% C and D matrix (measurement model)
+C = eye(7);
+D = zeros(7,1);
+
+sys_d = ss(A,B,C,D,Ts);
+
+res1 = sys_d.A*States_l + B*dot_delta_e
+
+%% Simulink
+
+% Time update in local frame
+states_dot = zeros(7,1);
+
+states_dot(1) = States(7);
+states_dot(2) = v * (States_l(3) + (lr/(lr+lf))*States(5) );
+states_dot(3) = (v/(lr+lf)) * States(5);
+states_dot(4) = States(5);
+states_dot(5) = (g/h)*States(4) + ((v^2*h-lr*g*c)/(h*(lr+lf)))*States(5) + ((lr*v)/(h^2*(lr+lf)))*dot_delta_e;
+states_dot(6) = dot_delta_e;
+states_dot(7) = 0;
+
+res2 = states_dot
