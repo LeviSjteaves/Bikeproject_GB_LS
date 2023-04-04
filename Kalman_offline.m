9235b8afec0a308b9c6a5cbcf8303b13e8dea3d5
LeviSjteaves <l.a.stevens@student.tue.nl>
2023-04-04 09:30:44 +0200

    .

--- Kalman_offline.m
+++ Kalman_offline.m
@@ -213,7 +213,34 @@ try Results = sim(model);
 end
 toc
 
+%% Plot results
+fig = figure()
+subplot(311)
+plot(Results.estimatedRoll.Time, rad2deg(Results.estimatedRoll.Data))
+hold on
+plot(data1.Time, rad2deg(data1.Roll))
+xlabel('Time (s)')
+ylabel('Roll (degree)')
+grid on
+legend('Kalman Sim', 'data')
 
+subplot(312)
+plot(Results.estimatedSteer_angle.Time, rad2deg(Results.estimatedSteer_angle.Data))
+hold on
+plot(data1.Time, rad2deg(data1.SteeringAngle - data1.steering_offset))
+xlabel('Time (s)')
+ylabel('Steering Angle (degree)')
+grid on
+legend('Kalman Sim', 'data')
+
+subplot(313)
+plot(Results.estimatedRoll_rate.Time, rad2deg(Results.estimatedRoll_rate.Data))
+hold on
+plot(data1.Time, rad2deg(data1.RollRate))
+xlabel('Time (s)')
+ylabel('Roll Rate (degree/s)')
+grid on
+legend('Kalman Sim', 'data')
 
 %% Utility Functions
 
